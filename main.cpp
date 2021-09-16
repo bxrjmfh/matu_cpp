f17c288f41dc3d70cdd98726257f44b45ef6902a
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-16 20:25:41 +0800

    100内能被3整除且个位为6（C++） 通过课程进入	题 号:	125	100内能被3整除且个位为6（C++）	语言要求：	C++ 下列程序的功能是：输出100以内能被3整除且个位数为6的所有整数，请填空： int main(){    int i,j;    for(i =0; ___①____;i++){        j = i*10 +6; 	 if(___②___)continue; 	 cout<<j;    }    return 0; }

--- main.cpp
+++ main.cpp
@@ -1,45 +1,11 @@
-#include <iostream>
-
-void print_star(int n, int m) {
-    int mid = (n - 1) / 2 + 1;
-    int star = (n - m) / 2;
-    int i = 1;
-
-    while (i <= star) {
-        printf(" ");
-        i++;
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i =0;i<=9;i++){
+        j = i*10 +6;
+        if((j%3)!=0)continue;
+        cout<<j;
     }
-    while (i < mid) {
-        printf("*");
-        i++;
-    }
-
-    printf("*");
-    i++;
-    while (i <= n-star) {
-        printf("*");
-        i++;
-    }
-    while (i <= n) {
-        printf(" ");
-        i++;
-    }
-    printf("\n");
-
-
-}
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    if (n % 2 == 0&&n>=1&&n<80) {
-        printf("error");
-        return 0;
-    } else {
-        for (int i = n; i > 0; i -= 2) {
-            print_star(n, i);
-        }
-        return 0;
-    }
-
+    return 0;
 }
