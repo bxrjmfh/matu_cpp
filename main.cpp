dd59ad7b4dddb8e5620a40ecdcafe5c2af8d050f
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-16 19:14:39 +0800

    输出n!（C++） 通过课程进入	题 号:	126	输出n!（C++）	语言要求：	C++ 求n!(n由键盘输入)，当结果将要超出表示范围时退出（以32位机器为例），显示溢出前的n以及n!结果。 输入输出示例： 输入: 5 输出: 5!=120

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
-
+int power(int n){
+    if (n==1){
+        return 1;
+    }
+    else{
+        int temp=n*power(n-1);
+        if(temp>=n)
+        {
+            return temp;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+        int n=0;
+        scanf("%d",&n);
+        if(!n>0){
+            if(n==0){printf("%d!=%d",n,1); }
+            else{return 0;}
+        }
+        else{
+            int temp=power(n);
+            if(n<13)
+            {
+                printf("%d!=%d",n,temp);
+            }
+            else if(n==13)
+            {
+                printf("%d!=%d",n,power(12));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
 }
