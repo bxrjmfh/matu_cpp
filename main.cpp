b0ce01b5d2552f34cc14430b8443b2968d883744
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-16 19:21:46 +0800

    将回车和制表符转成可见(C++) 通过课程进入	题 号:	130	将回车和制表符转成可见(C++)	语言要求：	C++ 函数expand(char *s ,char *t) 在将字符串s复制到字符串t时，将其中的换行符 和制表符转换为可见的转义字符，即用"\n"表示换行符，用"\t"表示制表符。请填空。 void expand (char *s ,char *t) {    for(int i =0, j=0;s[i] !='\0';i++)       switch(s[i]){ 	     case'\n';t[___①___] =____②___; 		          t[j++] ='n'; 				  ___③___; 		 case'\t';t[___④___] =____⑤___; 		          t[j++] ='t'; 				  break; 		default: t[____⑥___] = s[i]; 		          break; 	  } 	  t[j] =___⑦____; }

--- main.cpp
+++ main.cpp
@@ -1,44 +1,24 @@
 #include <iostream>
-int power(int n){
-    if (n==1){
-        return 1;
-    }
-    else{
-        int temp=n*power(n-1);
-        if(temp>=n)
-        {
-            return temp;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-}
+
 int main() {
 
+}
 
-        int n=0;
-        scanf("%d",&n);
-        if(!n>0){
-            if(n==0){printf("%d!=%d",n,1); }
-            else{return 0;}
+
+
+void expand (char *s ,char *t)
+{
+    for(int i =0, j=0;s[i] !='\0';i++){
+        switch(s[i]){
+            case'\n':t[j++] ='\\';
+            t[j++] ='n';
+            break;
+            case'\t':t[j++] ='\\';
+            t[j++] ='t';
+            break;
+            default: t[j++] = s[i];
+            break;
         }
-        else{
-            int temp=power(n);
-            if(n<13)
-            {
-                printf("%d!=%d",n,temp);
-            }
-            else if(n==13)
-            {
-                printf("%d!=%d",n,power(12));
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-
+        t[j] ='\0';
+    }
 }
