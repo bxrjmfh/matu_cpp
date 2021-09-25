90d7b26b2d136e89323017e31de565c2eac56e3c
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-25 19:35:50 +0800

    计算大整数的差（C++） 通过课程进入	题 号:	135	计算大整数的差（C++）	语言要求：	C++ 编写程序接受两个任意大的整数，返回两个整数之差， （提示：大整数用字符串表示）。 输出格式：如果结果为正，在结果前用"+"表示，如果为负数则加上"-"; 如果结果的位数小于输入整数的位数，则在结果前面补零直到和位数最大的输入数位数一致; 如果两个数相等直接输出0 例如输入： 22345678900 12345678900 输出： +10000000000 输入： 100 1010 输出： -0910 输入： 12345 12345 输出： 0
    屑码图没有string的pop_back(),需要使用erase()

--- main.cpp
+++ main.cpp
@@ -3,32 +3,67 @@
 #include<sstream>
 #include<vector>
 #include <cstring>
+#include <algorithm>
 
 
 using namespace std;
-int SubStrNum(char* str, char* substr) {
-    string Str = str;
-    string Substr = substr;
-    int counter = 0;
-    for (int i = 0; i <= Str.size() - Substr.size(); i++) {
-        int flag = 0;
-        for (int j = i; j-i < Substr.size(); j++) {
-            if (Substr[j - i] != Str[j]) {
-                flag = 1;
+string sminus(string s1, string s2) {
+    string out;
+    //    s1-s2
+    //   判断是不是需要交换
+    int alremain = 0;
+    int max = 0;
+    if(s1==s2){return "0";}
 
-                break;
-            }
+    if (s2.size() > s1.size() ||
+    (s1.size() == s2.size() && s2[0] > s1[0])) {
+        s1.swap(s2);
+        alremain = 1;
+    }
+    max = s1.size();
+    int remain = 0;
+
+    for (int i = s1.size()-1, j = s2.size()-1; i >= 0 && j >= 0; i--, j--) {
+        if (s1[i] < (s2[j] + remain)) {
+            out.push_back(s1[i] +10- (s2[j] + remain) + 48);
+            remain = 1;
         }
-        if (flag == 0) {
-            counter++;
-            i = i + Substr.size()-1;
+        else {
+            out.push_back(s1[i] - s2[j] - remain + 48);
+            remain = 0;
+        }
+        s1.erase(s1.size()-1,1);
+        s2.erase(s2.size()-1,1);
+    }
+    if (!s1.empty() && s2.empty())
+    {//被减数为空
+        while (!s1.empty()) {
+            if (s1[s1.size() - 1] == 48) {
+                out.push_back('9');
+                remain = 1;
+            }
+            else {
+                out.push_back(s1[s1.size() - 1]-remain);
+                remain = 0;
+            }
+            s1.erase(s1.size()-1,1);
         }
     }
-    cout << "match times=" << counter;
-    return counter;
+    while (out.size() < max) {
+        out.push_back('0');
+    }
+    if (alremain == 1) {
+        out.push_back('-');
+    }
+    else {
+        out.push_back('+');
+    }
+
+    reverse(out.begin(), out.end());
+    return out;
 }
 int main() {
-    char s1[] = "sadasadsadsasdssdasd";
-    char s2[] = "da";
-    int p= SubStrNum(s1,s2 );
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << sminus(s1, s2);
 }
