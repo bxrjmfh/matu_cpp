603c688c0d182b3bbd2625f6be982e2db36c7917
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-25 15:25:06 +0800

    矩阵乘法（C++） 通过课程进入	题 号:	133	矩阵乘法（C++）	语言要求：	C++ 实现函数 void Matrix_Mul(int a[3][2], int b[2][4]),计算两个矩阵的乘积（矩阵分别为3*2和 2*4）。 计算后输出结果矩阵，按行输出，行之间用回车隔开， 同一行的元素之间用空格间隔，每行最后一个元素后也有空格 例如： a ={{1,2},     b={{1,0,1,1},     {3,4},       {0,1,0,1}}     {5,6}} 输出： 1 2 1 3 回车 3 4 3 7 回车 5 6 5 11 回车
    交函数记得带上头文件

--- main.cpp
+++ main.cpp
@@ -6,14 +6,18 @@
 
 
 using namespace std;
-
-int main(){
-    string s1,s2;
-    cin>>s1;
-    for(int i=0;i<s1.size();i++){
-        s2.push_back(s1[i]);
+void Matrix_Mul(int a[3][2], int b[2][4]){
+    for(int j=0;j<3;j++){
+        for(int i=0;i<4;i++){
+            cout<<a[j][0]*b[0][i]+a[j][1]*b[1][i]<<" ";
+        }
+        cout<<endl;
     }
-    s2.push_back('\\');
-    s2.push_back('0');
-    cout<<s2;
+
+}
+int main(){
+    int a[3][2] ={{1,2}, {3,4},  {5,6}},   b[2][4]={{1,0,1,1},{0,1,0,1}};
+    Matrix_Mul(a,b);
+
+
 }
