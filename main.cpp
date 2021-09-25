9f3e7423010fc38484f862743d03ac0202bd709b
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-25 19:38:42 +0800

    输入两个参数和操作符并计算结果（C++） 通过课程进入	题 号:	124	输入两个参数和操作符并计算结果（C++）	语言要求：	C++ 下列程序接受从键盘输入的两个数以及+、-、*、/运算符，将两个数进行加、减、乘、除，输出运算结果。请填空： #include "iostream" #include <math.h> using namespace std; int main(){ char c; float a,b,result(0); int tag(1); //标志，1；合法，0：数据或操作不合法 cin>>a>>b>>c; switch(c){      case___①___:result = a+b; break; 	 case___②___:result = a-b;break; 	 case___③___:result = a*b;break; 	 case___④___: 	 if(fabs(b)<1e-6){ 	  cout<<"divide 0"<< endl;//注意0前面有空格 	  tag=0; 	  __⑤____ 	 } 	 result = a/b; 	 cout<<result<<endl; 	 break; 	 ___⑥___ 	 tag =0; 	 cout<<"invalid operation"<<endl; 	 ___⑦___ } if(tag){     cout<<result<<endl; } return 0; }

--- main.cpp
+++ main.cpp
@@ -1,69 +1,31 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
-#include<vector>
-#include <cstring>
-#include <algorithm>
-
-
+#include "iostream"
+#include <math.h>
 using namespace std;
-string sminus(string s1, string s2) {
-    string out;
-    //    s1-s2
-    //   判断是不是需要交换
-    int alremain = 0;
-    int max = 0;
-    if(s1==s2){return "0";}
-
-    if (s2.size() > s1.size() ||
-    (s1.size() == s2.size() && s2[0] > s1[0])) {
-        s1.swap(s2);
-        alremain = 1;
-    }
-    max = s1.size();
-    int remain = 0;
-
-    for (int i = s1.size()-1, j = s2.size()-1; i >= 0 && j >= 0; i--, j--) {
-        if (s1[i] < (s2[j] + remain)) {
-            out.push_back(s1[i] +10- (s2[j] + remain) + 48);
-            remain = 1;
+int main(){
+    char c;
+    float a,b,result(0);
+    int tag(1); //标志，1；合法，0：数据或操作不合法
+    cin>>a>>b>>c;
+    switch(c){
+        case'+':result = a+b; break;
+        case'-':result = a-b;break;
+        case'*':result = a*b;break;
+        case'/':
+        if(fabs(b)<1e-6){
+            cout<<"divide 0"<< endl;//注意0前面有空格
+            tag=0;
+            break;
         }
-        else {
-            out.push_back(s1[i] - s2[j] - remain + 48);
-            remain = 0;
-        }
-        s1.erase(s1.size()-1,1);
-        s2.erase(s2.size()-1,1);
+        result = a/b;
+        cout<<result<<endl;
+        break;
+        default:
+        tag =0;
+        cout<<"invalid operation"<<endl;
+            break;
     }
-    if (!s1.empty() && s2.empty())
-    {//被减数为空
-        while (!s1.empty()) {
-            if (s1[s1.size() - 1] == 48) {
-                out.push_back('9');
-                remain = 1;
-            }
-            else {
-                out.push_back(s1[s1.size() - 1]-remain);
-                remain = 0;
-            }
-            s1.erase(s1.size()-1,1);
-        }
+    if(tag){
+        cout<<result<<endl;
     }
-    while (out.size() < max) {
-        out.push_back('0');
-    }
-    if (alremain == 1) {
-        out.push_back('-');
-    }
-    else {
-        out.push_back('+');
-    }
-
-    reverse(out.begin(), out.end());
-    return out;
-}
-int main() {
-    string s1, s2;
-    cin >> s1 >> s2;
-    cout << sminus(s1, s2);
+    return 0;
 }
