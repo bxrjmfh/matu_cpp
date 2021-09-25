643e363c3ed170296e3a78c8b5bde95381acb600
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-25 15:10:44 +0800

    复制字符串，将'\0'转义（C++） 通过课程进入	题 号:	132	复制字符串，将'\0'转义（C++）	语言要求：	C++ 编写程序，输入一个字符数组s1，然后将字符数组s1中的全部字符复制到字符字符数组s2，不能用strcpy。 赋值时，'\0'也要复制过去，并表示成转义字符输出。 例如输入：hello 输出：hello\0

--- main.cpp
+++ main.cpp
@@ -2,47 +2,18 @@
 #include<fstream>
 #include<sstream>
 #include<vector>
+#include <cstring>
 
 
 using namespace std;
-void sort(vector<int>arr) {
-    for (int i = arr.size()-1; i >= 0; i--) {
-        for (int j = 0; j <= i - 1; j++) {
-            int temp;
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+
+int main(){
+    string s1,s2;
+    cin>>s1;
+    for(int i=0;i<s1.size();i++){
+        s2.push_back(s1[i]);
     }
-}
-int main()
-{
-    vector<int> arr;
-    int counter = 0;
-    int temp = -1;
-    do {
-        cin >> temp;
-        if (temp < 0) { break; }
-        arr.push_back(temp);
-        counter++;
-        if (counter == 10) { break; }
-    } while (1);
-
-    vector<int> arr_even, arr_odd;
-    for (int i=0;i<arr.size();i++) {
-        if (arr[i] % 2 == 0) {
-            arr_even.push_back(arr[i]);
-        }
-        else {
-            arr_odd.push_back(arr[i]);
-        }
-    }
-    sort(arr_odd);
-    sort(arr_even);
-
-    for (int i=0;i<arr_odd.size();i++ ) { cout << arr_odd[i] << " "; }
-    for (int i=0;i<arr_even.size();i++ ) { cout << arr_even[i] << " "; }
-
+    s2.push_back('\\');
+    s2.push_back('0');
+    cout<<s2;
 }
