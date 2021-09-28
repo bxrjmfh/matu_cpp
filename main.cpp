a2dc26bb49bf38685ab8229a14ec946accda5292
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-28 17:18:37 +0800

    实现Date类(C++) 通过课程进入	题 号:	139	实现Date类(C++)	语言要求：	C++ 实现日期类Date，它能正确表示年、月、日。 在代码中引入头文件 CDate.h 它的内容如下： class Date{  public:        Date(int y =1996,int m=1,int d=1){};        int days(int year,int month){};        void NewDay();  void display()  {     cout<<year<<"-"<<moth<<"-"<<day<<endl;  }  private:       int year; //年       int month; //月       int day;  // 日 }
    需要实现构成函数Date(int y =1996,int m=1,int d=1)
    在构造函数内依次判断输入的月和日是否合法
    即月为1~12月中的一个 如果月不合法则输出Invalid month! 然后将月置为1。
    如果天数不合法，则输出Invalid day! 然后将天数置为1。
    例如 Date d(1993,13,32);
    输出：
    Invalid month!回车
    Invalid day!回车
    再调用 d.display()
    输出：
    1993-1-1
    
    设计一个NewDay()成员函数，将当前Date表示的日期增加一天。
    例如：Date d(1991,1,1);
          d.NewDay();
          d.display();
    输出：
    1991-1-2
    
    设计一个days(int year, int month)成员函数,返回指定年月有多少天。
    例如 d.days(1991,1)
    返回：
    31
    
    在你的代码中必须实现以上三个方法，例如：
    #include "CDate.h"
    Date::Date(int y=1996,int m=1 ,int d=1){
        ...
    }
    int  Date::days(int year,int month){
        ...
    }
    void Date::NewDay(){
      ...
    }
    
    注意自己的头文件需要使用双引号

--- main.cpp
+++ main.cpp
@@ -1,34 +1,151 @@
 #include <iostream>
-#include <vector>
 #include <math.h>
+#include "CDate.h"
 
-using namespace std;
-class Ctriangle {
-    double a,b,c;
-    public:
-    Ctriangle(double a,double b,double c){
-        this->a=a;
-        this->b=b;
-        this->c=c;
+Date::Date(int y, int m, int d) {
+    if (m >= 1 && m <= 12) {
+        this->month = m;
+    } else {
+        cout << "Invalid month!"<<endl;
+        this->month = 1;
     }
-    void display(void){
-        cout<<"Ctriangle:a="<<a<<",b="<<b<<",c="<<c<<endl;
+    switch (this->month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            if (d >= 1 && d <= 31) {
+                this->day = d;
+            } else {
+                cout << "Invalid day!"<<endl;
+                this->day = 1;
+            }
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (d >= 1 && d <= 30) {
+                this->day = d;
+            } else {
+                cout << "Invalid day!";
+                this->day = 1;
+            }
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                if (d >= 1 && d <= 29) {
+                    this->day = d;
+                } else {
+                    cout << "Invalid day!";
+                    this->day = 1;
+                }
+            } else {
+                if (d >= 1 && d <= 28) {
+                    this->day = d;
+                } else {
+                    cout << "Invalid day!";
+                    this->day = 1;
+                }
+            }
+            break;
+        default:
+            break;
     }
-    double GetPerimeter(void){
-        return(a+b+c);
+    this->year=y;
+}
+
+int Date::days(int year, int month) {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                return 29;
+            } else {
+                return 28;
+            }
+            break;
+        default:
+            break;
     }
-    double GetArea(void){
-        double s=(a+b+c)/2;
-        double area=sqrt(s*(s-a)*(s-b)*(s-c));
-        return area;
+}
+
+void Date::NewDay() {
+    int month_flag = 0;
+    int year_flag = 0;
+
+    switch (this->month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            if (this->day == 31) {
+                this->day = 1;
+                month_flag++;
+            } else {
+                this->day += 1;
+            }
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (this->day == 30) {
+                this->day = 1;
+                month_flag++;
+            } else {
+                this->day += 1;
+            }
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                if (this->day == 29) {
+                    this->day = 1;
+                    month_flag++;
+                } else {
+                    this->day += 1;
+                }
+            } else {
+                if (this->day == 28) {
+                    this->day = 1;
+                    month_flag++;
+                } else {
+                    this->day += 1;
+                }
+            }
+            break;
+        default:
+            break;
     }
-};
-int main(){
-    double a,b,c;
-    cin>>a>>b>>c;
-    Ctriangle T(a,b,c);
-    T.display();
-    cout<<"Perimeter:"<<T.GetPerimeter()<<endl;
-    cout<<"Area:"<<T.GetArea()<<endl;
-    return 0;
+
+    if (this->month < 12) {
+        this->month += month_flag;
+    } else {
+        this->month = 1;
+        year_flag++;
+    }
+
+    this->year += year_flag;
 }
