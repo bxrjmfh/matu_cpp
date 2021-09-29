96580d62984f87eb81075e092914ea82950c8209
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-29 20:13:26 +0800

    （多态）Set类（C++） 通过课程进入	题 号:	165	（多态）Set类（C++）	语言要求：	C++ 引入头文件CSet.h,它的内容如下： #include <iostream> using namespace std; class Set{ private: 	int n; 	int * pS; //集合元素 public: 	Set(){n = 0;pS =NULL;} 	Set(Set &s){ 		n = s.n; 		if (n !=0) 		{ 			pS= new  int[n+1]; 			for (int i =1;i<=n;i++) //集合的下标从1开始，集合中不能有重复元素 				pS[i] = s.pS[i]; 		} 	} 	~Set(){ 		if (pS) 		{ 			delete []pS; 			pS = NULL; 			n =0; 		} 	} 		void ShowElement()const{ //输出集合的元素 		int temp = 0; 		for(int i=1;i<n;i++) 		{ 			for(int j=i+1;j<n;j++) 			{ 				if(pS[i] > pS[j]) 				{ 					temp = pS[i]; 					pS[i] = pS[j]; 					pS[j] = temp; 				} 			} 		} 		cout<<"{"; 		for(int i =1;i<n;i++) 			cout <<pS[i]<<","; 		if (IsEmpty()) 			cout<<"}"<<endl; 		else cout<<pS[n]<<"}"<<endl; 	} 	bool IsEmpty()const{return n?false:true;} //判断集合是否为空 	int size(){return n;} 	bool IsElement(int e)const { 		for (int i =1;i<=n;i++) 			if (pS[i] ==e) 			return true; 		return  false; 	} 	bool operator <=(const Set &s)const;//this <= s判断当前集合是否包于集合s 	bool operator ==(const Set &s)const; //判断集合是否相等 	Set & operator +=(int e);    // 向集合中增减元素e 	Set & operator -=(int e);    //删除集合中的元素e
    	Set operator |(const Set &s)const;  //集合并
    	Set operator &(const Set &s)const;//集合交
    	Set operator -(const Set &s)const; //集合差
    };
    完成Set类，实现运算符的重载。
    重载操作符+=,向集合中增减元素e,例如:
    Set s;
    s +=1;
    s.ShowElement();//{1}
    
    重载操作符-=,删除集合中元素e,例如:
    Set s;
    s +=1,s+=2;
    s.ShowElement();//{1,2}
    s -=1;
    s.showElement();//{2}
    
    重载操作符<=,判断当前集合是否包于另一个集合，例如:
    Set s1,s2,s3;
    s1 +=1; s2+=1;s2+=3; s3+=2;
    s1 <=s2;//true
    s3 <=s2//false;
    
    重载操作符==,判断集合是否相等,例如:
    Set s1 s2;
    s1 == s2;//true
    s1+=1;s2+=2;
    s1 ==s2 ;//false;
    
    重载操作符|,集合并,例如:
    Set s1 s2;
    s1+=1;s2+=2;
    s1|s2 ;//{1,2}
    
    重载操作符&,集合交,例如:
    Set s1 s2;
    s1+=1;s2+=2;s2+=1;
    s1&s2 ;//{1}
    
    重载操作符-,集合差,例如:
    Set s1 s2;
    s1+=1;s1+=3;s2+=2;s2+=1;
    s1-s2 ;//{3}
    
    注意空集、检测集合元素是否重复以及溢出

--- main.cpp
+++ main.cpp
@@ -1,16 +1,171 @@
 #include <iostream>
+#include "CSet.h"
+#include <vector>
+
 using namespace std;
-class Base{
-public:
-    virtual void fun(){cout<<1<<endl;}
-};
-class Derived:public Base{
-public:
-    void fun(){cout<<2<<endl;}
-};
-int main(){
-    Base *p = new Derived;
-    p->fun();
-    delete p;
-    return 0;
+
+Set& Set::operator+=(int e) {
+
+    int flag = 0;
+    for (int i = 1; i <= this->n; i++) {
+        if (this->pS[i] == e) {
+            flag += 1;
+            break;
+        }
+    }
+    if (flag == 0) {
+        int* temp_pS = new int[this->n + 2];
+        for (int i = 1; i <= this->n; i++) {
+            temp_pS[i] = this->pS[i];
+        }
+        temp_pS[n + 1] = e;
+        this->n = this->n + 1;
+        this->pS = temp_pS;
+    }
+
+    return *this;
+
+}
+
+Set& Set::operator-=(int e) {
+    int flag = 0;
+    if (this->n == 0) {
+        return *this;
+    }
+    for (int i = 1; i <= this->n; i++) {
+        if (this->pS[i] == e) {
+            flag += 1;
+            break;
+        }
+    }
+    if (flag == 1) {
+        int* temp = new int[n];
+        int j = 1;
+        for (int i = 1; i <= this->n; i++) {
+            if (this->pS[i] != e) {
+                temp[j++] = this->pS[i];
+            }
+        }
+        this->n -= 1;
+        this->pS = temp;
+    }
+
+    return *this;
+}
+
+bool Set::operator<=(const Set& s) const {
+    if (s.n < this->n) {
+        return false;
+    }
+    else if (this->n == 0) {
+        return true;
+    }
+    else {
+        int flag = 0;
+        bool status = false;
+        for (int i = 1; i <= this->n; i++) {
+            for (int j = 1; j <= s.n; j++) {
+                if (this->pS[i] == s.pS[j]) {
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                status = false;
+                break;
+            }
+            else {
+                flag = 0;
+                status = true;
+            }
+        } //集合的下标从1开始，集合中不能有重复元素
+        return status;
+    }
+}
+
+bool Set::operator==(const Set& s) const {
+    if (s.n != this->n) {
+        return false;
+    }
+    else if (s.n == 0) {
+        return true;
+    }
+    else {
+        int flag = 0;
+        bool status = false;
+        for (int i = 1; i <= this->n; i++) {
+            for (int j = 1; j <= s.n; j++) {
+                if (this->pS[i] == s.pS[j]) {
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                status = false;
+                break;
+            }
+            else {
+                flag = 0;
+                status = true;
+            }
+        } //集合的下标从1开始，集合中不能有重复元素
+        return status;
+    }
+}
+
+Set Set::operator|(const Set& s) const {
+    Set ret;
+    if (s == *this || *this <= s) {
+        for (int i = 1; i <= s.n; i++) {
+            ret += s.pS[i];
+        }
+
+    }
+    else {
+        for (int i = 1; i <= this->n; i++) {
+            ret += this->pS[i];
+        }
+        for (int i = 1; i <= s.n; i++) {
+            ret += s.pS[i];
+        }
+
+    }
+    return ret;
+}
+
+Set Set::operator&(const Set& s) const {
+    Set ret;
+    if (*this <= s) {
+        for (int i = 1; i <= this->n; i++) {
+            ret += this->pS[i];
+        }
+        return ret;
+    }
+    else {
+        for (int i = 1; i <= this->n; i++) {
+            for (int j = 1; j <= s.n; j++) {
+                if (this->pS[i] == s.pS[j]) {
+                    ret += this->pS[i];
+                }
+            }
+        } //集合的下标从1开始，集合中不能有重复元素
+    }
+    return ret;
+}
+
+Set Set::operator-(const Set& s) const {
+    Set ret;
+    vector<int> temp;
+    for (int i = 1; i <= this->n; i++) {
+        ret += this->pS[i];
+    } //集合的下标从1开始，集合中不能有重复元素
+    for (int i = 1; i <= s.n; i++) {
+        ret -= s.pS[i];
+    }
+    return ret;
+}
+
+int main() {
+    Set s1 ,s2;
+    s1 += 1; s1 += 3; s2 += 2; s2 += 1;
+    ;//{3}
+    (s1 - s2).ShowElement();
 }
