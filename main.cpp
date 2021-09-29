a6e0e11abe33c6fd0290114df6cb4d8eebd5b6f8
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-29 09:24:29 +0800

    继承Point类（C++） 通过课程进入	题 号:	150	继承Point类（C++）	语言要求：	C++ 引入CPoint.h头文件，它的内容如下： #include <iostream> using namespace std; class Point{ public: 	Point(float xx, float yy){ 		x = xx; 		y =yy; 	} private: 	float x; 	float y; }; class Rectangle :public Point{ public: 	Rectangle(float xx,float yy,float w,float h); 	float Area(); private: 	float width; 	float high; };
    class Circle:public Point{
    public:
    	Circle(float xx,float yy,float r);
    	float Area();
    private:
    	float radius;
    };
    实现Rectangle类和Circle类，它们都继承至Point类，派生类都具有float Area()方法，返回派生对象的面积。
    
    Rectangle类为矩形对象，拥有长和宽属性。
    Circle类为圆形，有有半径属性。
    
    完成Rectangle类和Circle类的构造方法和Area()方法
    
    最终两个类的使用方法如下：
    Rectangle rect(1,2,3,4);
    rect.Area();     //12
    Circle c(5,6,7);
    c.Area();      //153.86
    
    注意基类初始化的方式

--- main.cpp
+++ main.cpp
@@ -1,64 +1,18 @@
 #include <iostream>
 #include <math.h>
-#include "CString.h"
+#include "CPoint.h"
 
-bool String::IsSubstring(const char *str) {
-    string Substr = str;
-    string string_main = this->GetStr();
-    int flag = 0;
-    for (int i = 0; i <= this->len - Substr.size(); i++) {
-        for (int j = i; j - i < Substr.size(); j++) {
-            if (Substr[j - i] != string_main[j]) {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 0) {
-            i = i + Substr.size() - 1;
-        }
-    }
-    if (flag==0){
-        return true;
-    }else{
-        return false;
-    }
-};
-bool String::IsSubstring(const String &str) {
-    string Substr = str.mystr;
-    string string_main = this->GetStr();
-    int flag = 0;
-    for (int i = 0; i <= this->len - Substr.size(); i++) {
-        for (int j = i; j - i < Substr.size(); j++) {
-            if (Substr[j - i] != string_main[j]) {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 0) {
-            i = i + Substr.size() - 1;
-        }
-    }
-    if (flag==0){
-        return true;
-    }else{
-        return false;
-    }
-};
-int String::str2num() {
-    int out=0;
-    string str=this->GetStr();
-    for(int i=0;i<this->len;i++){
-        if(str[i]>='0'&&str[i]<='9'){
-            i=i*10+(int)str[i]-48;
-        }
-    }
-    return out;
+Rectangle ::Rectangle(float xx, float yy, float w, float h) : Point(xx, yy) {
+    width=w;
+    high=h;
 }
-void String::toUppercase(){
-    string str=this->GetStr();
-    for(int i=0;i<this->len;i++){
-        if(str[i]>='a'&&str[i]<='z'){
-            str[i]+=32;
-        }
-    }
+float Rectangle::Area() {
+    return width*high;
+}
+
+Circle ::Circle(float xx, float yy, float r) : Point(xx,yy){
+    radius=r;
+}
+float Circle::Area() {
+    return(radius*radius*3.14);
 }
