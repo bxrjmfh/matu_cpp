af1676369c1de549b23e3317a0ce0c42b83541d4
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-03 20:35:53 +0800

    ClockWithDate类（C++） 通过课程进入	题 号:	155	ClockWithDate类（C++）	语言要求：	C++ 引入头文件ClockAndDate.h，它的内容如下： #include <iostream> using namespace std; class Clock{ public: 	Clock(int h,int m,int s){ 	 hour =(h>23? 0:h); 	 minute = (m>59?0:m); 	 second = (s>59?0:m); 	} 	virtual void run(){ 		second = second+1; 		if (second>59) 		{ 			second =0; 			minute+=1; 		} 		if (minute>59) 		{ 			minute =0; 			hour+=1; 		} 		if (hour>23) 		{ 			hour =0; 		} 	} 	virtual void showTime(){ 		cout<<"Now:"<<hour<<":"<<minute<<":"<<second<<endl; 	} 	int getHour(){return hour;} 	int getMinute(){return minute;} 	int getSecond(){return second;}
    Clock * createClockWithDate(int h,int m,int s,int year,int month,int day);
    protected:
    	int hour;
    	int minute;
    	int second;
    };
    
    class Date{
    public:
    	Date(int y=1996,int m=1,int d=1){
    		day =d;
    		year =y;
    		month =m;
    		if (m>12||m<1)
    		{
    			m=1;
    		}
    		if(d>days(y,m)){
    			day = 1;
    		}
    	};
    	int days(int year,int month);
    	void NewDay();
    	void display(){
    		cout<<year<<"-"<<month<<"-"<<day<<endl;
    	}
    protected:
    	int year;
    	int month;
    	int day;
    };
    
    需要实现Date类的days方法，根据年和月，返回该年该月对应的天数
    实现Date类的NewDay方法，该方法将Date代表的日期增加一天。
    
    实现ClockWithDate类，它继承至Clock类和Date类，记录时间和日期
    重新实现ClockWithDate类的showTime方法和run方法。
    showTime方法输出当的时间和日期，先输出时间再输出日期。
    run方法每次将现在的时间增加一秒，并且当时间超过23：59：59时，更新日期。
    
    必须实现Clock类的createClockWithDate方法，它的实现必须在ClockWithDate类的定义之后，它的内容如下：
    Clock* Clock::createClockWithDate(int h,int m,int s,int year,int month,int day){
    	return new ClockWithDate(h,m,s,year,month,day);
    }

--- main.cpp
+++ main.cpp
@@ -1,28 +1,131 @@
 #include <iostream>
 #include <cstring>
-#include "Clock.h"
+#include "ClockAndDate.h"
+
 using namespace std;
 
-class NewClock:public Clock{
-public:
-    NewClock(int h,int m,int s):Clock(h,m,s){
+int Date::days(int year, int month) {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+            break;
 
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                return 29;
+            } else {
+                return 28;
+            }
+            break;
+        default:
+            break;
     }
-    virtual void showTime(void) override{
-        int h=getHour();
-        int m=getMinute();
-        int s=getSecond();
+}
 
-        if(h<12){
-            cout<<"Now:"<<h<<":"<<m<<":"<<s<<"AM"<<endl;
-        }else if(h==12){
-            cout<<"Now:"<<h<<":"<<m<<":"<<s<<"PM"<<endl;
-        }else{
-            cout<<"Now:"<<h-12<<":"<<m<<":"<<s<<"PM"<<endl;
+void Date::NewDay() {
+    int month_flag = 0;
+    int year_flag = 0;
+
+    switch (this->month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            if (this->day == 31) {
+                this->day = 1;
+                month_flag++;
+            } else {
+                this->day += 1;
+            }
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (this->day == 30) {
+                this->day = 1;
+                month_flag++;
+            } else {
+                this->day += 1;
+            }
+            break;
+        case 2:
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                if (this->day == 29) {
+                    this->day = 1;
+                    month_flag++;
+                } else {
+                    this->day += 1;
+                }
+            } else {
+                if (this->day == 28) {
+                    this->day = 1;
+                    month_flag++;
+                } else {
+                    this->day += 1;
+                }
+            }
+            break;
+        default:
+            break;
+    }
+
+    if (this->month < 12) {
+        this->month += month_flag;
+    } else {
+        this->month = 1;
+        year_flag++;
+    }
+
+    this->year += year_flag;
+}
+
+class ClockWithDate:public Clock,public Date{
+public:
+    void showTime() override{
+        cout<<"Now:"<<hour<<":"<<minute<<":"<<second<<endl;
+        cout<<year<<"-"<<month<<"-"<<day<<endl;
+    }
+    void run() override{
+        int now_h=getHour();
+        second = second+1;
+        if (second>59)
+        {
+            second =0;
+            minute+=1;
         }
+        if (minute>59)
+        {
+            minute =0;
+            hour+=1;
+        }
+        if (hour>23)
+        {
+            hour =0;
+        }
+        if(now_h>getHour()){
+            NewDay();
+        }
+    }
+    ClockWithDate(int h,int m,int s,int year,int month,int day): Clock(h,m,s), Date(year,month,day){
 
     }
 };
-Clock* Clock::createNewClock(int h,int m,int s){
-    return new NewClock(h,m,s);
+Clock* Clock::createClockWithDate(int h,int m,int s,int year,int month,int day){
+    return new ClockWithDate(h,m,s,year,month,day);
 }
