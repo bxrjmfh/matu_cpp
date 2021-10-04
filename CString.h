934c7b1ec3d76cad74f2ab52fcb74cce8c367999
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-04 15:27:58 +0800

    扩展String类（C++） 通过课程进入	题 号:	156	扩展String类（C++）	语言要求：	C++ 扩展String类，引入头文件CString.h,它的内容如下： #include <iostream> using namespace std; class String{ protected: 	char *mystr; 	int len; public: 	String(const char *p){ 		len = strlen(p); 		mystr = new char[len+1]; 		strcpy(mystr,p); 	} 	~String(){ 		if (mystr !=NULL) 		{ 			delete []mystr; 			mystr = NULL; 			len = 0; 		} 	} 	void showStr(){cout <<mystr;} 	char * GetStr(){return mystr;} 	virtual bool IsSubString(const char *str){ 		int i,j; 		for (i =0;i<len;i++) 		{ 			int k = i; 			for (j =0;str[j] !='\0';j++,k++) 			{ 				if (str[j]!= mystr[k]) break; 			} 			if(str[j] =='\0') return true; 		} 		return false; 	} };
    class EditString:public String{
    public:
    	EditString(const char *p):String(p){}
    	int IsSubString(int start, const char *str);
    	void EditChar(char s, char d);
    	void EditSub(char * subs,char *subd);
    
    	void DeleteChar(char ch);
    	void DeleteSub(char * sub);
    
    };
    
    实现int IsSubString(int start, const char *str);
    从start处开始判断str是否为字符串的子串，是则返回子串第一次出现处的下标位置(下标从0开始计算)，否则返回-1；
    例如：
    EditString es("testtesttest");
    es.IsSubString(3,"test");//4
    
    实现EditChar(char s, char d)，用字符d代替所有字符s，例如：
    EditString es("testtesttest");
    es.EditChar('t','a');//"aesaaesaaesa"
    
    实现void EditSub(char * subs,char *subd)，用字符串subd代替所有字符串subs，例如：
    EditString es("testtesttest");
    es.EditChar('test','str');//"strstrstr"
    
    实现void DeleteChar(char ch)，将所有ch字符删除，例如：
    EditString es("testtesttest");
    es.DeleteChar('t');//"eseses"
    
    实现void DeleteSub(char * sub)，将所有的字符串sub删除，例如：
    EditString es("testtesttest");
    es.DeleteSub('es');//"tttttt";
    
    需要注意string的copy方法要在拷贝到的字符串后边加上结束字符，否则cout的时候无法判断结束条件
    方法中多次使用了IsSubstring(),注意考虑strat的范围

--- CString.h
+++ CString.h
@@ -7,44 +7,52 @@
 
 #endif //CPP_CSTRING_H
 #include <iostream>
-#include <string>
 #include <cstring>
-
 using namespace std;
 class String{
-private:
-    char *mystr;    //字符串
-    int len;    //字符串长度
+protected:
+    char *mystr;
+    int len;
 public:
-    String(){
-        len =0;
-        mystr =NULL;
-        if (mystr != NULL)
-        {
-            delete []mystr;
-            mystr =NULL;
-            len = 0;
-        }
-    }
-    String(const char* p){
+    String(const char *p){
         len = strlen(p);
         mystr = new char[len+1];
         strcpy(mystr,p);
     }
-    String(String &s){
-        len = s.len;
-        if (len !=0)
+    ~String(){
+        if (mystr !=NULL)
         {
-            mystr = new char[len+1];
-            strcpy(mystr,s.mystr);
+            delete []mystr;
+            mystr = NULL;
+            len = 0;
         }
     }
+    void showStr(){cout <<mystr;}
+    char * GetStr(){return mystr;}
+    virtual bool IsSubString(const char *str){
+        int i,j;
+        for (i =0;i<len;i++)
+        {
+            int k = i;
+            for (j =0;str[j] !='\0';j++,k++)
+            {
+                if (str[j]!= mystr[k]) break;
+            }
+            if(str[j] =='\0') return true;
+        }
+        return false;
+    }
 
-    char *GetStr(){return mystr;}
-    void ShowStr(){cout<<mystr;}
+};
 
-    bool IsSubstring(const char *str);
-    bool IsSubstring(const String &str);
-    int str2num();
-    void toUppercase();
+class EditString:public String{
+public:
+    EditString(const char *p):String(p){}
+    int IsSubString(int start, const char *str);
+    void EditChar(char s, char d);
+    void EditSub(char * subs,char *subd);
+
+    void DeleteChar(char ch);
+    void DeleteSub(char * sub);
+
 };
--- main.cpp
+++ main.cpp
@@ -1,131 +1,121 @@
 #include <iostream>
-#include <cstring>
-#include "ClockAndDate.h"
+#include "CString.h"
+#include <string>
 
 using namespace std;
 
-int Date::days(int year, int month) {
-    switch (month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            return 31;
-            break;
-
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-            break;
-        case 2:
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-                return 29;
-            } else {
-                return 28;
+int EditString::IsSubString(int start, const char* str) {
+    string Substr = str;
+    string string_main = this->GetStr();
+    if (start > len - Substr.size()) {
+        return -1;
+    }
+    else {
+        int flag = 0;
+        for (int i = start ; i <= this->len - Substr.size()+1; i++) {
+            flag = 0;
+            for (int j = i; j - i < Substr.size(); j++) {
+                if (Substr[j - i] != string_main[j]) {
+                    flag = 1;
+                    break;
+                }
             }
-            break;
-        default:
-            break;
+            if (flag == 0) {
+                return i;
+            }
+        }
+
+        if (flag != 0) {
+            return -1;
+        }
     }
 }
 
-void Date::NewDay() {
-    int month_flag = 0;
-    int year_flag = 0;
-
-    switch (this->month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            if (this->day == 31) {
-                this->day = 1;
-                month_flag++;
-            } else {
-                this->day += 1;
-            }
-            break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            if (this->day == 30) {
-                this->day = 1;
-                month_flag++;
-            } else {
-                this->day += 1;
-            }
-            break;
-        case 2:
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-                if (this->day == 29) {
-                    this->day = 1;
-                    month_flag++;
-                } else {
-                    this->day += 1;
-                }
-            } else {
-                if (this->day == 28) {
-                    this->day = 1;
-                    month_flag++;
-                } else {
-                    this->day += 1;
-                }
-            }
-            break;
-        default:
-            break;
+void EditString::EditChar(char s, char d) {
+    for (int i = 0; i < len; i++) {
+        if (mystr[i] == s) {
+            mystr[i] = d;
+        }
     }
-
-    if (this->month < 12) {
-        this->month += month_flag;
-    } else {
-        this->month = 1;
-        year_flag++;
-    }
-
-    this->year += year_flag;
 }
 
-class ClockWithDate:public Clock,public Date{
-public:
-    void showTime() override{
-        cout<<"Now:"<<hour<<":"<<minute<<":"<<second<<endl;
-        cout<<year<<"-"<<month<<"-"<<day<<endl;
-    }
-    void run() override{
-        int now_h=getHour();
-        second = second+1;
-        if (second>59)
-        {
-            second =0;
-            minute+=1;
-        }
-        if (minute>59)
-        {
-            minute =0;
-            hour+=1;
-        }
-        if (hour>23)
-        {
-            hour =0;
-        }
-        if(now_h>getHour()){
-            NewDay();
-        }
-    }
-    ClockWithDate(int h,int m,int s,int year,int month,int day): Clock(h,m,s), Date(year,month,day){
+void EditString::EditSub(char* subs, char* subd) {
+    string change_to = subd;
+    string to_change = subs;
+    string final_str;
 
+    int location = 0;
+    while (location<len) {
+        int temp = IsSubString(location, subs);
+        if (temp >= 0) {
+            for (int i = location; i < temp; i++) {
+                final_str.push_back(mystr[i]);
+            }
+            for (int i = 0; i < change_to.size(); i++) {
+                final_str.push_back(change_to[i]);
+            }
+            location = temp+to_change.size();
+        }
+        else {
+            final_str.push_back(mystr[location]);
+            location++;
+        }
     }
-};
-Clock* Clock::createClockWithDate(int h,int m,int s,int year,int month,int day){
-    return new ClockWithDate(h,m,s,year,month,day);
+    this->len =final_str.size();
+    char* tep_char = new char[len+1];
+    final_str.copy(tep_char, len);
+    tep_char[len] = '\0';
+    this->mystr = tep_char;
+    return;
+}
+
+void EditString::DeleteChar(char ch) {
+    string temp;
+    int counter = 0;
+    for (int i = 0; i < len; i++) {
+        if (mystr[i] == ch) {
+            counter++;
+            continue;
+        }
+        temp.push_back(mystr[i]);
+    }
+    this->len -= counter;
+    char* tep_char = new char[len+1];
+    temp.copy(tep_char, len);
+    tep_char[len] = '\0';
+    this->mystr = tep_char;
+}
+
+void EditString::DeleteSub(char* sub) {
+    string to_del = sub;
+    string final_str;
+
+    int location = 0;
+    while (location < len) {
+        int temp = IsSubString(location, sub);
+        if (temp >= 0) {
+            for (int i = location ; i < temp; i++) {
+                final_str.push_back(mystr[i]);
+            }
+            location = temp + to_del.size();
+        }
+        else {
+            final_str.push_back(mystr[location]);
+            location++;
+        }
+    }
+    this->len = final_str.size();
+    char* tep_char = new char[len + 1];
+    final_str.copy(tep_char, len);
+    tep_char[len] = '\0';
+    this->mystr = tep_char;
+    return;
+}
+
+int main() {
+
+    EditString es("testtesttest");
+
+    es.DeleteSub("es");
+    es.showStr();
 }
