7eac04350a1502257f9d6ad6010a85b15a78f64b
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-21 21:33:26 +0800

    异常捕获 通过课程进入	题 号:	527	异常捕获	语言要求：	C++ 下列程序的功能是：输入数组下标和除数，从数组中取出下标对应的数组元素作为被除数，输出除法运算的结果。例如， 输入：9 5 输出：2
    捕获程序数组下标越界错误，给出相应的提示，例如，
    输入：10 5
    输出：10 out of bound
    
    输入：-1 5
    输出：-1 out of bound
    
    捕获程序数组下标除零错误，给出相应的提示，例如，
    输入：9 0
    输出：divide by 0
    
    部分代码已给出，请将代码补充完整。
    #include <iostream>
    
    using namespace std;
    
    int main()
    
    {
    
      int index, dividend, divisor, result;
    
      int array[10]={1,2,3,4,5,6,7,8,9,10};
    
      try
    
      {
    
        cin >> index >> divisor;
    
        if( index < 0 || index>9)
    
          throw index;
    
        if(divisor==0)
    
          throw divisor;
    
        dividend = array[index];
    
        result = dividend / divisor;
    
        cout << result << endl;
    
      }
    
      return 0;
    
    }

--- main.cpp
+++ main.cpp
@@ -2,166 +2,44 @@
 
 using namespace std;
 
-template<typename T>
-class node {
-    T num;
-    node *next;
-    node *prev;
-public:
-    node(){
-        num=0;
-        next= NULL;
-        prev= NULL;
-    }
-    void set_value(T set_num) {
-        num = set_num;
-    }
+int main()
 
-    void append(node *another) {
-        next = another;
-    }
+{
 
-    node *get_prev() {
-        return prev;
-    }
+    int index, dividend, divisor, result;
 
-    node *get_next() {
-        return next;
-    }
+    int array[10]={1,2,3,4,5,6,7,8,9,10};
 
-    T get_value() {
-        return num;
+    try
+
+    {
+
+        cin >> index >> divisor;
+
+        if( index < 0 || index>9)
+
+            throw index;
+
+        if(divisor==0)
+
+            throw divisor;
+
+        dividend = array[index];
+
+        result = dividend / divisor;
+
+        cout << result << endl;
+
     }
-    void insert(node * to_be){
-        if(this->prev){
-            this->prev->next=to_be;
-            to_be->prev= this->prev;
-            to_be->next= this;
-            this->prev=to_be;
-        }else{
-            to_be->next= this;
-            this->prev=to_be;
+    catch (int index) {
+        if(index==0){
+            cerr <<"divide by 0" << endl;
         }
 
-    }
-};
+        else{
+            cerr << index<<" out of bound" << endl;
 
-#include <iostream>
-
-using namespace std;
-
-void test1() {
-
-    node<int> *ptr;
-
-    node<int> node1, node2, node3;
-
-    node1.set_value(1);
-
-    node2.set_value(2);
-
-    node3.set_value(3);
-
-    node3.insert(&node2);
-
-    node2.insert(&node1);
-
-    for (ptr = &node1;; ptr = ptr->get_next()) {
-
-        cout << ptr->get_value() << " ";
-
-        if (ptr->get_next() == NULL) {
-            break;
         }
-
-    }
-
-}
-
-void test2() {
-
-    node<float> *ptr;
-
-    node<float> node1, node2, node3;
-
-    node1.set_value(1.1);
-
-    node2.set_value(2.2);
-
-    node3.set_value(3.3);
-
-    node3.insert(&node2);
-
-    node2.insert(&node1);
-
-    for (ptr = &node1;; ptr = ptr->get_next()) {
-
-        cout << ptr->get_value() << " ";
-
-        if (ptr->get_next() == NULL) {
-            break;
-        }
-
-    }
-
-}
-
-void test3() {
-
-    node<char> *ptr;
-
-    node<char> node1, node2, node3;
-
-    node1.set_value('a');
-
-    node2.set_value('b');
-
-    node3.set_value('c');
-
-    node3.insert(&node2);
-
-    node2.insert(&node1);
-
-    for (ptr = &node1;; ptr = ptr->get_next()) {
-
-        cout << ptr->get_value() << " ";
-
-        if (ptr->get_next() == NULL) {
-            break;
-        }
-
-    }
-
-}
-
-int main() {
-
-    int type;
-
-    cin >> type;
-
-
-    switch (type) {
-
-        case 1:
-
-            test1();
-
-            break;
-
-
-        case 2:
-
-            test2();
-
-            break;
-
-        case 3:
-
-            test3();
-
-            break;
-
     }
 
     return 0;
