6f33e94d745bb8b437e40bf0e6dc9fe065ba1180
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-21 20:32:35 +0800

    异常处理 通过课程进入	题 号:	528	异常处理	语言要求：	C++ 下列程序的功能是：输入数组下标和除数，从数组中取出下标对应的数组元素作为被除数，输出除法运算的结果。例如， 输入：9 5 输出：2
    捕获程序数组下标越界错误，给出相应的提示，例如，
    输入：10 5
    输出：10 out of bound
    
    输入：-1 5
    输出：-1 out of bound
    
    捕获程序数组下标除零错误，给出相应的提示，例如，
    输入：9 0
    输出：divide by 0
    
    部分代码已给出，请将代码补充完整。
    #include <iostream>
    
    using namespace std;
    
    int main()
    
    {
    
      int index, dividend, divisor, result;
    
      int array[10]={1,2,3,4,5,6,7,8,9,10};
    
      try
    
      {
    
        cin >> index >> divisor;
    
        if( index < 0 || index>9)
    
          throw A(index);
    
        if(divisor==0)
    
          throw B(divisor);
    
        dividend = array[index];
    
        result = dividend / divisor;
    
        cout << result << endl;
    
      }
    
      catch(A a)
    
      {
    
        cout << a.err << " out of bound" << endl;
    
      }
    
      catch(B b)
    
      {
    
        cout << "divide by "<< b.err << endl;
    
      }
    
      return 0;
    
    }

--- main.cpp
+++ main.cpp
@@ -1,142 +1,71 @@
 #include <iostream>
 
 using namespace std;
-
-template<typename T >class node{
-    T num;
-    node* next;
-    node* prev;
+class A {
 public:
-    void set_value(T set_num){
-        num=set_num;
-    }
-    void append(node * another){
-        next=another;
-    }
-    node* get_prev(){
-        return prev;
-    }
-    node* get_next(){
-        return next;
-    }
-    T get_value(){
-        return num;
+    int err;
+
+    A(int err){
+        this->err=err;
     }
 
 };
-void test1() {
+class B {
+public:
+    int err;
 
-    node<int> *ptr;
+    B(int err){
+        this->err=err;
+    }
 
-    node<int> node1, node2, node3;
+};
+int main()
 
-    node1.set_value(1);
+{
 
-    node2.set_value(2);
+    int index, dividend, divisor, result;
 
-    node3.set_value(3);
+    int array[10]={1,2,3,4,5,6,7,8,9,10};
 
-    node1.append(&node2);
+    try
 
-    node2.append(&node3);
+    {
 
-    for (ptr = &node1;; ptr = ptr->get_next()) {
+        cin >> index >> divisor;
 
-        cout << ptr->get_value() << " ";
+        if( index < 0 || index>9)
 
-        if (ptr->get_next() == NULL) {
-            break;
-        }
+            throw A(index);
+
+        if(divisor==0)
+
+            throw B(divisor);
+
+        dividend = array[index];
+
+        result = dividend / divisor;
+
+        cout << result << endl;
 
     }
 
-}
+    catch(A a)
 
-void test2() {
+    {
 
-    node<float> *ptr;
-
-    node<float> node1, node2, node3;
-
-    node1.set_value(1.1);
-
-    node2.set_value(2.2);
-
-    node3.set_value(3.3);
-
-    node1.append(&node2);
-
-    node2.append(&node3);
-
-    for (ptr = &node1;; ptr = ptr->get_next()) {
-
-        cout << ptr->get_value() << " ";
-
-        if (ptr->get_next() == NULL) {
-            break;
-        }
+        cout << a.err << " out of bound" << endl;
 
     }
 
-}
+    catch(B b)
 
-void test3() {
+    {
 
-    node<char> *ptr;
-
-    node<char> node1, node2, node3;
-
-    node1.set_value('a');
-
-    node2.set_value('b');
-
-    node3.set_value('c');
-
-    node1.append(&node2);
-
-    node2.append(&node3);
-
-    for (ptr = &node1;; ptr = ptr->get_next()) {
-
-        cout << ptr->get_value() << " ";
-
-        if (ptr->get_next() == NULL) {
-            break;
-        }
+        cout << "divide by "<< b.err << endl;
 
     }
 
-}
 
-int main() {
-
-    int type;
-
-    cin >> type;
-
-
-    switch (type) {
-
-        case 1:
-
-            test1();
-
-            break;
-
-
-        case 2:
-
-            test2();
-
-            break;
-
-        case 3:
-
-            test3();
-
-            break;
-
-    }
 
     return 0;
 
