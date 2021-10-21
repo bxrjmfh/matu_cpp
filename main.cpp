9e44767e3bfdeb14b4fb1605769d13ab5f807330
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-21 19:52:36 +0800

    get_sum 通过课程进入	题 号:	522	get_sum	语言要求：	C++ 编写一个模板函数get_sum，接收两个参数：数组和数组的长度，返回该数组所有元素的和。部分代码已给出，请将代码补充完整。 #include <iostream>
    using namespace std;
    
    int main()
    
    {
    
      int arr_int[6] = { 1, 2, 3, 4, 5, 6 };
    
      double arr_double[6] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
    
      cout << get_sum(arr_int, 6) << endl;
    
      cout << get_sum(arr_double, 6) << endl;
    
      return 0;
    
    }

---
+++ main.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T> T get_sum(T* arr,int size){
+    T temp=arr[0];
+    for(int i=1;i<size;i++){
+        temp+=arr[i];
+    }
+    return temp;
+}
+
+int main()
+
+{
+
+    int arr_int[6] = { 1, 2, 3, 4, 5, 6 };
+
+    double arr_double[6] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+
+    cout << get_sum(arr_int, 6) << endl;
+
+    cout << get_sum(arr_double, 6) << endl;
+
+    return 0;
+
+}
