a30f414ae82d955c0090cdc522cd75474c1ebf3b
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-21 21:05:17 +0800

    node insert 通过课程进入	题 号:	526	node insert	语言要求：	C++ 编写一个表示链表节点的模板类node，实现链表节点的以下功能： set_value函数：设置节点值 get_value函数：获取节点值 get_prev函数：获取上一个节点 get_next函数：获取下一个节点 insert函数：在当前节点的位置插入新节点（当前节点后移）
    例如，下列程序中，
    test1函数的输出应为：1 2 3
    test2函数的输出应为：1.1 2.2 3.3
    test3函数的输出应为：a b c
    
    部分代码已给出，请将代码补充完整

--- main.cpp
+++ main.cpp
@@ -2,27 +2,170 @@
 
 using namespace std;
 
-template<typename T>T get_min(T* arr,int size){
-    T temp=arr[0];
-    for (int i = 1; i < size; ++i) {
-        if(arr[i]<temp){
-            temp=arr[i];
-        }
+template<typename T>
+class node {
+    T num;
+    node *next;
+    node *prev;
+public:
+    node(){
+        num=0;
+        next= NULL;
+        prev= NULL;
     }
-    return temp;
+    void set_value(T set_num) {
+        num = set_num;
+    }
+
+    void append(node *another) {
+        next = another;
+    }
+
+    node *get_prev() {
+        return prev;
+    }
+
+    node *get_next() {
+        return next;
+    }
+
+    T get_value() {
+        return num;
+    }
+    void insert(node * to_be){
+        if(this->prev){
+            this->prev->next=to_be;
+            to_be->prev= this->prev;
+            to_be->next= this;
+            this->prev=to_be;
+        }else{
+            to_be->next= this;
+            this->prev=to_be;
+        }
+
+    }
+};
+
+#include <iostream>
+
+using namespace std;
+
+void test1() {
+
+    node<int> *ptr;
+
+    node<int> node1, node2, node3;
+
+    node1.set_value(1);
+
+    node2.set_value(2);
+
+    node3.set_value(3);
+
+    node3.insert(&node2);
+
+    node2.insert(&node1);
+
+    for (ptr = &node1;; ptr = ptr->get_next()) {
+
+        cout << ptr->get_value() << " ";
+
+        if (ptr->get_next() == NULL) {
+            break;
+        }
+
+    }
+
 }
-int main()
 
-{
+void test2() {
 
-    int arr_int[6] = { 1, 2, 3, 4, 5, 6 };
+    node<float> *ptr;
 
-    double arr_double[6] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+    node<float> node1, node2, node3;
 
-    cout << get_max(arr_int, 6) << endl;
+    node1.set_value(1.1);
 
-    cout << get_max(arr_double, 6) << endl;
+    node2.set_value(2.2);
+
+    node3.set_value(3.3);
+
+    node3.insert(&node2);
+
+    node2.insert(&node1);
+
+    for (ptr = &node1;; ptr = ptr->get_next()) {
+
+        cout << ptr->get_value() << " ";
+
+        if (ptr->get_next() == NULL) {
+            break;
+        }
+
+    }
+
+}
+
+void test3() {
+
+    node<char> *ptr;
+
+    node<char> node1, node2, node3;
+
+    node1.set_value('a');
+
+    node2.set_value('b');
+
+    node3.set_value('c');
+
+    node3.insert(&node2);
+
+    node2.insert(&node1);
+
+    for (ptr = &node1;; ptr = ptr->get_next()) {
+
+        cout << ptr->get_value() << " ";
+
+        if (ptr->get_next() == NULL) {
+            break;
+        }
+
+    }
+
+}
+
+int main() {
+
+    int type;
+
+    cin >> type;
+
+
+    switch (type) {
+
+        case 1:
+
+            test1();
+
+            break;
+
+
+        case 2:
+
+            test2();
+
+            break;
+
+        case 3:
+
+            test3();
+
+            break;
+
+    }
 
     return 0;
 
 }
+
+
