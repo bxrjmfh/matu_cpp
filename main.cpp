1ebbede072e4a7f2dee9aec9b5431bf5f03c0208
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-10-21 20:11:19 +0800

    node append 通过课程进入	题 号:	525	node append	语言要求：	C++ 编写一个表示链表节点的模板类node，实现链表节点的以下功能： set_value函数：设置节点值 get_value函数：获取节点值 get_prev函数：获取上一个节点 get_next函数：获取下一个节点 append函数：在当前节点之后追加新节点
    例如，下列程序中，
    test1函数的输出应为：1 2 3
    test2函数的输出应为：1.1 2.2 3.3
    test3函数的输出应为：a b c
    
    部分代码已给出，请将代码补充完整。
    
    给出的代码：
    #include <iostream>
    
    using namespace std;
    
    void test1()
    
    {
    
      node<int> *ptr;
    
      node<int> node1,node2,node3;
    
      node1.set_value(1);
    
      node2.set_value(2);
    
      node3.set_value(3);
    
      node1.append(&node2);
    
      node2.append(&node3);
    
      for(ptr=&node1 ; ; ptr=ptr->get_next())
    
      {
    
        cout << ptr->get_value() << " ";
    
        if(ptr->get_next()==NULL) break;
    
      }
    
    }
    
    void test2()
    
    {
    
      node<float> *ptr;
    
      node<float> node1,node2,node3;
    
      node1.set_value(1.1);
    
      node2.set_value(2.2);
    
      node3.set_value(3.3);
    
      node1.append(&node2);
    
      node2.append(&node3);
    
      for(ptr=&node1 ; ; ptr=ptr->get_next())
    
      {
    
        cout << ptr->get_value() << " ";
    
        if(ptr->get_next()==NULL) break;
    
      }
    
    }
    
    void test3()
    
    {
    
      node<char> *ptr;
    
      node<char> node1,node2,node3;
    
      node1.set_value('a');
    
      node2.set_value('b');
    
      node3.set_value('c');
    
      node1.append(&node2);
    
      node2.append(&node3);
    
      for(ptr=&node1 ; ; ptr=ptr->get_next())
    
      {
    
        cout << ptr->get_value() << " ";
    
        if(ptr->get_next()==NULL) break;
    
      }
    
    }
    
    int main( )
    
    {
    
      int type;
    
      cin >> type;
    
      switch(type)
    
      {
    
        case 1:
    
        test1();
    
        break;
    
        case 2:
    
        test2();
    
        break;
    
        case 3:
    
        test3();
    
        break;
    
      }
    
    return 0;
    
    }

--- main.cpp
+++ main.cpp
@@ -1,28 +1,145 @@
-
 #include <iostream>
 
 using namespace std;
 
-template<typename T> T get_sum(T* arr,int size){
-    T temp=arr[0];
-    for(int i=1;i<size;i++){
-        temp+=arr[i];
+template<typename T >class node{
+    T num;
+    node* next;
+    node* prev;
+public:
+    void set_value(T set_num){
+        num=set_num;
     }
-    return temp;
+    void append(node * another){
+        next=another;
+    }
+    node* get_prev(){
+        return prev;
+    }
+    node* get_next(){
+        return next;
+    }
+    T get_value(){
+        return num;
+    }
+
+};
+void test1() {
+
+    node<int> *ptr;
+
+    node<int> node1, node2, node3;
+
+    node1.set_value(1);
+
+    node2.set_value(2);
+
+    node3.set_value(3);
+
+    node1.append(&node2);
+
+    node2.append(&node3);
+
+    for (ptr = &node1;; ptr = ptr->get_next()) {
+
+        cout << ptr->get_value() << " ";
+
+        if (ptr->get_next() == NULL) {
+            break;
+        }
+
+    }
+
 }
 
-int main()
+void test2() {
 
-{
+    node<float> *ptr;
 
-    int arr_int[6] = { 1, 2, 3, 4, 5, 6 };
+    node<float> node1, node2, node3;
 
-    double arr_double[6] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+    node1.set_value(1.1);
 
-    cout << get_sum(arr_int, 6) << endl;
+    node2.set_value(2.2);
 
-    cout << get_sum(arr_double, 6) << endl;
+    node3.set_value(3.3);
+
+    node1.append(&node2);
+
+    node2.append(&node3);
+
+    for (ptr = &node1;; ptr = ptr->get_next()) {
+
+        cout << ptr->get_value() << " ";
+
+        if (ptr->get_next() == NULL) {
+            break;
+        }
+
+    }
+
+}
+
+void test3() {
+
+    node<char> *ptr;
+
+    node<char> node1, node2, node3;
+
+    node1.set_value('a');
+
+    node2.set_value('b');
+
+    node3.set_value('c');
+
+    node1.append(&node2);
+
+    node2.append(&node3);
+
+    for (ptr = &node1;; ptr = ptr->get_next()) {
+
+        cout << ptr->get_value() << " ";
+
+        if (ptr->get_next() == NULL) {
+            break;
+        }
+
+    }
+
+}
+
+int main() {
+
+    int type;
+
+    cin >> type;
+
+
+    switch (type) {
+
+        case 1:
+
+            test1();
+
+            break;
+
+
+        case 2:
+
+            test2();
+
+            break;
+
+        case 3:
+
+            test3();
+
+            break;
+
+    }
 
     return 0;
 
 }
+
+
